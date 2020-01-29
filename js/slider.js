0b650ccb6a74d3a6b0e54140109066da5b093b33
NaibRT <naibrt@code.edu.az>
2020-01-29 10:59:53 +0400

    changed slider to owl-carousel

--- js/slider.js
+++ js/slider.js
@@ -17,87 +17,60 @@ $(document).ready(() => {
         slideLent.style.left = `-${id*330}px`;
     }
 
-    function sliderItemSize() {
-        let width = $(document).outerWidth();
-
-        if (width <= 992 && width >= 768) {
-            let actualWidth = ((width - 50) / 3);
-            let btnWidth = (actualWidth / 2) - 10;
-            document.querySelectorAll('.m-shows-main-item').forEach(x =>
-                x.style.width = `${actualWidth}px`
-            )
-            $('.msmc-left,.msmc-right').css('width', `${actualWidth}`)
-        }
-        if (width <= 768 && width >= 576) {
-            let actualWidth = ((width - 30) / 2);
-            let btnWidth = (actualWidth / 2) - 10;
-            document.querySelectorAll('.m-shows-main-item').forEach(x =>
-                x.style.width = `${actualWidth}px`
-            )
-            $('.msmc-left,.msmc-right').css('width', `${actualWidth}`)
-        }
-        if (width <= 576) {
-            let actualWidth = ((width - 20));
-            let btnWidth = (actualWidth / 2) - 10;
-            document.querySelectorAll('.m-shows-main-item').forEach(x =>
-                x.style.width = `${actualWidth}px`
-            )
-            $('.msmc-left,.msmc-right').css('width', `${actualWidth}`)
-        }
-        if (width >= 992 && width < 1050) {
-            let actualWidth = ((width - 40) / 3);
-            let btnWidth = (actualWidth / 2) - 10;
-            document.querySelectorAll('.m-shows-main-item').forEach(x =>
-                x.style.width = `${actualWidth}px`
-            )
-            $('.msmc-left,.msmc-right').css('width', `${actualWidth}`)
-        }
-    }
-
-    sliderControllers.forEach(x => x.addEventListener('click', getindex));
-
-    let x = 0;
-    let clickCount = 1;
-
-    function scroll(target) {
-        let msmiWidth = (document.querySelector('.m-shows-main-item').offsetWidth);
-        let msmWidth = (document.querySelector('.m-shows-main').offsetWidth);
-        let msmLentWidth = (document.querySelector('.msm-lent').offsetWidth);
-        let msmLeft = (document.querySelector('.msm-lent').offsetLeft * -1);
-        let msm = document.querySelector('.msm-lent');
-        let msmScrollLeft = $('.m-shows-main').scrollLeft();
-        let scrollleft = msmScrollLeft - ((Math.floor(msmScrollLeft / msmiWidth)) * (msmiWidth + 10));
-
-        if (target === 'left' && ((msmLeft + msmiWidth) < (msmLentWidth - msmWidth))) {
-            clickCount += 1;
-            if (msmScrollLeft !== 0) {
-
-                x -= (msmScrollLeft + (msmiWidth - scrollleft) + 10);
-                $('.m-shows-main').scrollLeft(0);
-            } else {
-                x -= (msmiWidth + 10);
+   
+    $('.msm-lent').owlCarousel({
+        loop:false,
+     
+        margin:10,
+        nav:true,
+        navText:[
+            `<svg id="arrow-left-line" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <defs>
+              <style>
+                .cls-1 {
+                  fill: none;
+                }
+          
+                .cls-2 {
+                  fill: #fff;
+                }
+              </style>
+            </defs>
+            <path id="Path_272" data-name="Path 272" class="cls-1" d="M0,0H24V24H0Z"/>
+            <path id="Path_273" data-name="Path 273" class="cls-2" d="M7.828,11H20v2H7.828l5.364,5.364-1.414,1.414L4,12l7.778-7.778,1.414,1.414Z"/>
+          </svg>`,
+            `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <defs>
+              <style>
+                .cls-1 {
+                  fill: none;
+                }
+          
+                .cls-2 {
+                  fill: #fff;
+                }
+              </style>
+            </defs>
+            <g id="arrow" transform="translate(92 1502) rotate(180)">
+              <rect id="Rectangle_140" data-name="Rectangle 140" class="cls-1" width="24" height="24" transform="translate(68 1478)"/>
+              <path id="Path_273" data-name="Path 273" class="cls-2" d="M7.828,11H20v2H7.828l5.364,5.364-1.414,1.414L4,12l7.778-7.778,1.414,1.414Z" transform="translate(68 1478)"/>
+            </g>
+          </svg>`
+        ],
+        responsive:{
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            1050:{
+                items:3
             }
-            msm.style.left = `${x}px`;
         }
-        if (target === 'right' && (msmLeft > 0)) {
-            clickCount -= 1;
-            if (msmScrollLeft !== 0) {
-                x += (msmScrollLeft - (msmiWidth - scrollleft) + 10);
-                // $('.m-shows-main')[0].scrollLeft = 0;
-            } else {
-                x += (msmiWidth + 10);
-            }
-
-            msm.style.left = `${x}px`;
-        }
-    };
-    $('.msmc-left').click(() => {
-        scroll('right')
-    });
-    $('.msmc-right').click(() => {
-        scroll('left')
-    });
-
-    sliderItemSize();
+    })
 
 });
